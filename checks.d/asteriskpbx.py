0f8c9dfbc44f046417385d53c2512c2501478a1c
AZCPRDMIPBX01 <asterisk@azcprdmipbx01>
2022-11-04 13:35:49 +0000

    Fixing asterisk pbx

--- checks.d/asteriskpbx.py
+++ checks.d/asteriskpbx.py
@@ -70,19 +70,31 @@ class AsteriskCheck(AgentCheck):
             return
 
 ##### Call Volume
-        call_volume = mgr.command('core show calls')
+        self.log.error("asterisk check starting call volume")
+        try:
+            call_volume = mgr.command('core show calls')
 
-        current_call_vol = call_volume.data.split('\n')
+            current_call_vol = call_volume.data.split('\n')
+            procesed_call_vol = current_call_vol[1].replace('Output: ', '')
+            procesed_call_vol = procesed_call_vol.replace(' calls processed','')
+            current_call_vol = current_call_vol[0].replace('active call','')
+            current_call_vol = current_call_vol.replace('s','')
+            current_call_vol = current_call_vol.replace(' ','')
+            current_call_vol = current_call_vol.replace('Output:','')
 
-        procesed_call_vol = current_call_vol[1].replace(' calls processed','')
-        current_call_vol = current_call_vol[0].replace('active call','')
-        current_call_vol = current_call_vol.replace('s','')
-        current_call_vol = current_call_vol.replace(' ','')
-
-        self.gauge('asterisk.callsprocesed',procesed_call_vol)
-        self.gauge('asterisk.callvolume',current_call_vol)
-
+            self.gauge('asterisk.callsprocesed',procesed_call_vol)
+            self.gauge('asterisk.callvolume',current_call_vol)
+        
+        except asterisk.manager.ManagerSocketException as e:
+            self.log.info("Error connecting to the manager: %s" % e.strerror)
+    
+        except asterisk.manager.ManagerAuthException as e:
+            self.log.info("Error logging in to the manager: %s" % e.strerror)
+    
+        except asterisk.manager.ManagerException as e:
+       	    self.log.info("Error: %s" % e.strerror)
 ##### Internal, Inbound Outbound Calls
+        self.log.error("asterisk check starting internal call volume")
 
         extensionLength = instance['extension_length']
 
@@ -170,121 +182,27 @@ class AsteriskCheck(AgentCheck):
         self.gauge('asterisk.calls.conference',conferenceCalls)
 
 ##### PJSIP Endpoints
+        self.log.error("asterisk check starting PJSIP endpoints")
+
         sip_result = mgr.command('pjsip show endpoints')
 
         if "No such command" not in sip_result.data:
-
             sip_results = sip_result.data.split('\n')
 
             siptotals = sip_results[len(sip_results)-3]
 
             siptotal = re.findall(r'Objects found: ([0-9]+)',siptotals)[0]
-
-            monitored_peers_online = siptotal.count(" Avail ")
-            monitored_peers_offline = siptotal.count(" UnAvail ")
-         
+            monitored_peers_online = sip_result.data.count(" Avail ")
+            monitored_peers_offline = sip_result.data.count(" UnAvail ")
+	         
             self.gauge('asterisk.pjsip.peers',siptotal)
             self.gauge('asterisk.pjsip.monitored.online',monitored_peers_online)
             self.gauge('asterisk.pjsip.monitored.offline',monitored_peers_offline)
 
 
 
-##### PRI In Use
-
-        pri = mgr.command('pri show channels')
-
-        if "No such command" not in pri.data:
-
-            pri_channels = pri.data.split('\n')
-
-            pri_channels[0] = None
-            pri_channels[1] = None
-
-            openchannels = 0
-            for chan in pri_channels:
-                if chan != None:
-                    chan_data = chan.split()
-                    if len(chan_data) > 2 and chan_data[3] == "No":
-                        openchannels += 1
-
-            self.gauge('asterisk.pri.channelsinuse',openchannels)
-
-##### IAX2 Peers
-
-        iax_result = mgr.command('iax2 show peers')
-
-        if "No such command" not in iax_result.data:
-
-            iax_results = iax_result.data.split('\n')
-
-            iax_total_line = iax_results[len(iax_results)-3]
-
-            iax_peers_total = re.findall(r'([0-9]+) iax2 peers',iax_total_line)[0]
-            iax_peers_online = re.findall(r'\[([0-9]+) online',iax_total_line)[0]
-            iax_peers_offline = re.findall(r'([0-9]+) offline',iax_total_line)[0]
-            iax_peers_unmonitored = re.findall(r'([0-9]+) unmonitored',iax_total_line)[0]
-
-            self.gauge('asterisk.iax2.peers',iax_peers_total)
-            self.gauge('asterisk.iax2.online',iax_peers_online)
-            self.gauge('asterisk.iax2.offline',iax_peers_offline)
-            self.gauge('asterisk.iax2.unmonitored',iax_peers_unmonitored)
-   
-##### DAHDI Channels  
-    
-        dahdi_result = mgr.command('dahdi show status')
-
-        if "No such command" not in dahdi_result.data:
-
-            dahdi_results = dahdi_result.data.split('\n')
-
-            dahdi_total_trunks = len(dahdi_results)-3
-
-            dahdi_results[0] = None
-
-            dahdi_online_trunks = 0
-            dahdi_offline_trunks = 0
-
-            for chan in dahdi_results:
-                if chan != None:
-                    chan_data = chan.split()
-
-                    if len(chan_data) > 1:
-                        if "Wildcard" in chan_data[0]:
-                            if len(chan_data) > 2 and chan_data[2] == "OK":
-                                dahdi_online_trunks += 1
-                            if len(chan_data) > 2 and chan_data[2] == "RED":
-                                dahdi_offline_trunks += 1
-
-                        if "wanpipe" in chan_data[0]:
-                            if len(chan_data) > 2 and chan_data[3] == "OK":
-                                dahdi_online_trunks += 1
-                            if len(chan_data) > 2 and chan_data[3] == "RED":
-                                dahdi_offline_trunks += 1
-
-            self.gauge('asterisk.dahdi.total',dahdi_total_trunks)
-            self.gauge('asterisk.dahdi.online',dahdi_online_trunks)
-            self.gauge('asterisk.dahdi.offline',dahdi_offline_trunks)
-        
-##### G729 Codecs 
-        
-        g729_result = mgr.command('g729 show licenses')
-
-        if "No such command" not in g729_result.data:
-
-            g729_results = g729_result.data.split('\n')
-
-            g729_total_line = g729_results[0]
-
-            g729_total = re.findall(r'([0-9]+) licensed',g729_total_line)[0]
-            g729_encoders = re.split('/',g729_total_line)[0]
-            g729_decoders = re.findall(r'([0-9]+) encoders/decoders',g729_total_line)[0]
-
-            self.gauge('asterisk.g729.total',g729_total)
-            self.gauge('asterisk.g729.encoders',g729_encoders)
-            self.gauge('asterisk.g729.decoders',g729_decoders)
-        
-
 ##### Asterisk Uptime
+        self.log.error("asterisk check starting uptime")
 
         uptime_result = mgr.command('core show uptime')
         
@@ -333,62 +251,7 @@ class AsteriskCheck(AgentCheck):
         self.gauge('asterisk.system.uptime',system_uptime)
         self.gauge('asterisk.last.reload',asterisk_last_reload)
         
-##### MFCR2 Channels
-
-        mfcr2_result = mgr.command('mfcr2 show channels')
-
-        if "No such command" not in mfcr2_result.data:
-
-            mfcr2_results = mfcr2_result.data.split('\n')
-
-            mfcr2_total_channels = len(mfcr2_results)-3
-
-            mfcr2_results[0] = None
-
-            mfcr2_inuse_channels = 0
-            mfcr2_available_channels = 0
-            mfcr2_blocked_channels = 0
-
-            for chan in mfcr2_results:
-                if chan != None:
-                    chan_data = chan.split()
-                    if len(chan_data) > 2:
-                        if "IDLE" in chan_data[6] and "IDLE" in chan_data[7] :
-                            mfcr2_available_channels += 1
-                        if "ANSWER" in chan_data[6] or "ANSWER" in chan_data[7] :
-                            mfcr2_inuse_channels += 1
-                        if "BLOCK" in chan_data[6] or "BLOCK" in chan_data[7] :
-                            mfcr2_blocked_channels += 1
-
-            self.gauge('asterisk.mfcr2.total.channels',mfcr2_total_channels)
-            self.gauge('asterisk.mfcr2.available.channels',mfcr2_available_channels)
-            self.gauge('asterisk.mfcr2.inuse.channels',mfcr2_inuse_channels)
-            self.gauge('asterisk.mfcr2.blocked.channels',mfcr2_blocked_channels)
-
-##### SCCP Devices
-
-        sccp_total_devices = 0
-        sccp_online_devices = 0
-        sccp_offline_devices = 0
-        
-        sccp_result = mgr.command('sccp show devices')
-
-        if "No such command" not in sccp_result.data:
-            
-            sccp_devices = re.finditer('^.*.SEP.*', sccp_result.data, re.MULTILINE)
-
-            for sccp_device in sccp_devices:
-                sccp_total_devices +=1
-                if '--' in sccp_device.group():
-                    sccp_offline_devices += 1
-                else:
-                    sccp_online_devices += 1
-
-        self.gauge('asterisk.sccp.devices.total',sccp_total_devices)
-        self.gauge('asterisk.sccp.devices.online',sccp_online_devices)
-        self.gauge('asterisk.sccp.devices.offline',sccp_offline_devices)
-                    
-
 ##### Close connection
+        self.log.error("asterisk closing mgr connection")
 
         mgr.close()
